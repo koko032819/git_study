23de46c180941bab59c0520736629652db43ff64
misa <koko032819@naver.com>
2026-07-17 14:45:35 +0900

    fix

---
+++ basic_syntax/2.env_setting.sh
@@ -0,0 +1,35 @@
+# GitHub 인증방법 2가지
+# 1. oauth 인증방식 (제3자인증:웹로그인)
+# 2. PAT token (GitHub에서 직접 보안키 발급)
+# 발급한 키를 키체인(자격증명)에 등록
+
+# Git 프로젝트 생성방법 2가지
+# 방법 1. GitHub에서 진행 중인 원격 repo를 clone
+# 방법 2. 로컬에서부터 이미 진행 중인 프로젝트를 GitHub에 업로드
+# 해당 프로젝트 위치에 .git 폴더(repo 주소, 사용자 정보 등) 생성
+git init
+
+# 원격지 주소를 생성 및 추가
+git remote add origin 레포주소
+# 원격지 주소 삭제
+git remote remove origin
+# 원격지 주소 변경
+git remote set-url origin 레포주소
+# git 설정정보 조회
+git config --list
+
+# 타인의 레포를 clone 받는 방법 2가지
+# 1. 커밋이력 그대로 가져오기
+git clone 남의레포주소
+# 해당 폴더로 이동 후 GitHub 레포주소 변경
+git remote set-url origin 나의레포주소
+git push origin main
+
+# 2. 커밋이력 없이 레포 가져오기
+git clone 남의레포주소
+# 해당 폴더 안으로 이동 후 .git 폴더 삭제
+git init
+git remote add origin 나의레포주소
+git add .
+git commit -m "first"
+git push origin main
