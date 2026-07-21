7fb5202d80295fec48d25b7e1c03a4f9ea863567
koko <koko032819@naver.com>
2026-07-21 21:59:21 +0900

    add+commit

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -8,3 +8,18 @@ git commit -m "메세지"
 # origin : 원격공간
 # main : branch
 git push origin main
+
+# 현재 working directory, staging area의 상태 조회
+git status
+
+# 특정 파일만 add할 경우
+ git add 특정파일(위치포함)
+
+ # local repository에 커밋이력 생성
+ git commit -m "title" -m "message"
+
+ # vi에디터 모드에서 작성 : 첫 줄에는 제목, 두번째 줄부터 내용
+ git commit
+
+# add와 commit을 동시에
+ git commit -am "message"
