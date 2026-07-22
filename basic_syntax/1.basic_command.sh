980c2a7e36a9257237416826b289b98a5eaa3623
koko <koko032819@naver.com>
2026-07-22 22:04:49 +0900

    add java

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -23,3 +23,18 @@ git status
 
 # add와 commit을 동시에
  git commit -am "message"
+
+ # local repository의 commit 이력 조회
+ git log
+
+ # git 로그를 간결하게 조회
+ git log --oneline
+
+# git 로그를 그래프 형태로 조회
+ git log --graph
+
+# main 브랜치 뿐만 아니라 전체 commit 이력 조회
+ git log --all
+
+ # 충돌 무시하고 강제 push
+ git push origin main --force
